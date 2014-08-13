48d9f2f2646a83e684db2614fa6bba2647df84a5
James Hurlbut <james@stimulant.io>
2014-08-13 14:51:43 -0700

    reworking file structure

---
+++ src/include/NVPTextBox.h
@@ -0,0 +1,124 @@
+#pragma once
+
+#include "cinder/Cinder.h"
+
+#include "cinder/Surface.h"
+#include "cinder/Font.h"
+#include "cinder/Vector.h"
+
+#include <vector>
+#include <deque>
+#include <string>
+
+#include "cinder\gl\gl.h"
+#include "NVPFont.h"
+
+namespace cinder {
+	class NVPTextBox;
+	typedef boost::shared_ptr<class NVPTextBox>	NVPTextBoxRef; 
+
+class NVPTextBox {
+
+	
+  public:
+	static NVPTextBoxRef create()
+    {
+        return ( NVPTextBoxRef )( new NVPTextBox() );
+    }
+	typedef enum Alignment { LEFT, CENTER, RIGHT } Alignment;
+	enum { GROW = 0 };
+	
+	NVPTextBox();
+
+	NVPTextBox&			size( Vec2i sz ) { setSize( sz ); return *this; }
+	NVPTextBox&			size( int width, int height ) { setSize( Vec2i( width, height ) ); return *this; }
+	Vec2i				getSize() const { return mSize; }
+	void				setSize( Vec2i sz ) { mSize = sz; mInvalid = true; }
+
+	NVPTextBox&		text( const std::string &t ) { setText( t ); return *this; }
+	const std::string&	getText() const { return mText; }
+	void				setText( const std::string &t ) { mText = t; mInvalid = true; }
+	void				appendText( const std::string &t ) { mText += t; mInvalid = true; }
+
+	
+	NVPTextBox&		alignment( Alignment align ) { setAlignment( align ); return *this; }
+	Alignment			getAlignment() const { return mAlign; }
+	void				setAlignment( Alignment align ) { mAlign = align; mInvalid = true; }
+
+	NVPTextBox&		backgroundColor( ColorA bgColor ) { setBackgroundColor( bgColor ); return *this; }
+	ColorA				getBackgroundColor() const { return mBackgroundColor; }
+	void				setBackgroundColor( ColorA bgColor ) { mBackgroundColor = bgColor; }
+
+	NVPTextBox&			premultiplied( bool premult = true ) { setPremultiplied( premult ); return *this; }
+	bool				getPremultiplied() const { return mPremultiplied; }
+	void				setPremultiplied( bool premult ) { mPremultiplied = premult; }
+
+	NVPTextBox&			ligate( bool ligateText = true ) { setLigate( ligateText ); return *this; }
+	bool				getLigate() const { return mLigate; }
+	void				setLigate( bool ligateText ) { mLigate = ligateText; }
+
+	void				draw( Vec2f offset = Vec2f::zero() );
+	void				render();
+	void				setFont(std::string pSystemFontName, int pFontSize);
+	ColorA				getFillColor(){ return mFillColor; }
+	void				setFillColor(ColorA pCol){ mFillColor = pCol; }
+	ColorA				getStrokeColor(){ return mStrokeColor; }
+	void				setStrokeColor(ColorA pCol){ mStrokeColor = pCol; }
+	void				setKerning(float pKern){ mKerningFactor = pKern; mInvalid = true;}
+	float				getKerning(){ return mKerningFactor; }
+	void				setFilling(bool pFill){ mFilling = pFill; }
+	void				setUnderline(bool pUnder){ mUnderline = pUnder; }
+
+	void				setFont(NVPFontRef pFont){ mFont = pFont; mInvalid = true;}
+	void				setFontPt(float pSize){ mFontPt = pSize;}
+	float				getFontPt(){ return mFontPt; }
+	void				setDebugDraw(bool pDraw){ mDebugDraw = pDraw; }
+	float mScaleVal;
+  protected:
+	Alignment		mAlign;
+	Vec2i			mSize;
+	std::string		mText;
+	ColorA			mFillColor, mStrokeColor, mBackgroundColor;
+	float			mKerningFactor;
+
+	int				mFilling;
+	int				mUnderline;
+
+	bool			mPremultiplied;
+	bool			mLigate;
+	mutable bool	mInvalid;
+
+	mutable Vec2f	mCalculatedSize;
+	 
+	size_t mMessageLen;
+	GLfloat *xtranslate;
+
+    uint32_t mPathDefMode;
+   
+
+	GLfloat mTotalAdvance, mXBorder;
+	/* Global variables */
+	
+	GLuint mPathObj;
+	GLint sRGB_capable;
+	int hasFramebufferSRGB;
+	
+	int mRegularAspect;
+	int mFillGradient;
+	int emScale;
+
+	GLuint mPathTemplate;
+
+	NVPFontRef	mFont;
+
+	bool	mDrawBoundingBox;
+	bool	mDrawGlyphBounds;
+
+	float mFontPt;
+	bool	mDebugDraw;
+
+	float mInitialOffset;
+
+};
+
+}
